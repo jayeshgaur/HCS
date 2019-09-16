3098f5407e41ff68c8fc2fc8acac15b5719a7ee1
Jayesh Gaur <jayesh.gaur@somaiya.edu>
2019-09-16 18:13:50 +0530

    pushed test cases for admin center and test functions, except faildelete

--- HealthCareSystem/src/test/java/com/cg/healthcaresystem/test/CenterTest.java
+++ HealthCareSystem/src/test/java/com/cg/healthcaresystem/test/CenterTest.java
@@ -11,12 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.cg.healthcaresystem.dto.DiagnosticCenter;
+import com.cg.healthcaresystem.exception.UserDefinedException;
 import com.cg.healthcaresystem.service.UserService;
 import com.cg.healthcaresystem.service.UserServiceImpl;
 
 class CenterTest {
 	UserService userService;
-	static DiagnosticCenter diagnosticCenterA;
+	DiagnosticCenter diagnosticCenterA;
 	DiagnosticCenter diagnosticCenterE;
 	com.cg.healthcaresystem.dto.Test testA;
 	com.cg.healthcaresystem.dto.Test testE;
@@ -63,24 +64,32 @@ class CenterTest {
 		assertFalse(userService.removeCenter(diagnosticCenterA.getCenterId()));
 	}
 	
-//	@Test
-//	void addTest(){
-//		DiagnosticCenter diagnosticCenterA = new DiagnosticCenter("Center Name", "Center Address", BigInteger.valueOf(1234567890));
-//		DiagnosticCenter diagnosticCenterE = userService.addCenter(diagnosticCenterA);
-//		testA = (com.cg.healthcaresystem.dto.Test) new com.cg.healthcaresystem.dto.Test();
-//		testE = userService.addTest(diagnosticCenterE.getCenterId(), testA);
-//		assertEquals(testE, testA);
-//	}
-//	
-//	@Test
-//	void removeTest() {
-//		assertTrue(userService.removeTest(diagnosticCenterE.getCenterId(), testE.getTestId()));
-//	}
-//	
-//	@Test
-//	void failRemoveTest() {
-//		assertFalse(userService.removeTest(diagnosticCenterE.getCenterId(), testE.getTestId()));
-//	}
+	@Test
+	void addTest() throws UserDefinedException{
+		DiagnosticCenter diagnosticCenterA = new DiagnosticCenter("Center Name", "Center Address", BigInteger.valueOf(1234567890));
+		DiagnosticCenter diagnosticCenterE = userService.addCenter(diagnosticCenterA);
+		testA = (com.cg.healthcaresystem.dto.Test) new com.cg.healthcaresystem.dto.Test("Junit Test");
+		testE = userService.addTest(diagnosticCenterE.getCenterId(), testA);
+		assertEquals(testE, testA);
+	}
+	
+	@Test
+	void removeTest() throws UserDefinedException {
+		DiagnosticCenter diagnosticCenterA = new DiagnosticCenter("Center Name", "Center Address", BigInteger.valueOf(1234567890));
+		DiagnosticCenter diagnosticCenterE = userService.addCenter(diagnosticCenterA);
+		testA = (com.cg.healthcaresystem.dto.Test) new com.cg.healthcaresystem.dto.Test("Junit Test");
+		testE = userService.addTest(diagnosticCenterE.getCenterId(), testA);
+		assertTrue(userService.removeTest(diagnosticCenterE.getCenterId(), testE.getTestId()));
+	}
+	
+	@Test
+	void failRemoveTest() throws UserDefinedException{
+		DiagnosticCenter diagnosticCenterA = new DiagnosticCenter("Center Name", "Center Address", BigInteger.valueOf(1234567890));
+		DiagnosticCenter diagnosticCenterE = userService.addCenter(diagnosticCenterA);
+		testA = (com.cg.healthcaresystem.dto.Test) new com.cg.healthcaresystem.dto.Test("Junit Test");
+		testE = userService.addTest(diagnosticCenterE.getCenterId(), testA);
+		assertThrows(UserDefinedException.class, ()->userService.removeTest(diagnosticCenterE.getCenterId(), testE.getTestId()));
+	}
 	
 	
 
