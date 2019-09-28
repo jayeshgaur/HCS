eb6e52b2c45ceb5877367c3f075e1a64a4864159
jayeshgaur <jayesh.gaur@somaiya.edu>
2019-09-28 16:59:34 +0530

    learning git, request controller changes

--- HealthCareSystem/src/com/cg/healthcaresystem/controller/HCSController.java
+++ HealthCareSystem/src/com/cg/healthcaresystem/controller/HCSController.java
@@ -4,8 +4,10 @@ import javax.xml.ws.RequestWrapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cg.healthcaresystem.service.UserService;
 
@@ -18,6 +20,12 @@ public class HCSController {
 	@RequestMapping(value="loginPage", method=RequestMethod.GET)
 	public String loginpage() {
 		return "Login";
+	}
+	
+	
+	@RequestMapping(value="registerPage", method = RequestMethod.POST)
+	public String register() {
+		return "AA";
 	}
 
 }
