a77c830d26a218195fbb1fbaaeb1c7955d3dac6f
jayeshgaur <jayesh.gaur@somaiya.edu>
2019-09-28 16:31:44 +0530

    Added skeleton for spring

--- HealthCareSystem/src/com/cg/healthcaresystem/controller/HCSController.java
+++ HealthCareSystem/src/com/cg/healthcaresystem/controller/HCSController.java
@@ -1,7 +1,11 @@
 package com.cg.healthcaresystem.controller;
 
+import javax.xml.ws.RequestWrapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cg.healthcaresystem.service.UserService;
 
@@ -10,5 +14,10 @@ public class HCSController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@RequestMapping(value="loginPage", method=RequestMethod.GET)
+	public String loginpage() {
+		return "Login";
+	}
 
 }
