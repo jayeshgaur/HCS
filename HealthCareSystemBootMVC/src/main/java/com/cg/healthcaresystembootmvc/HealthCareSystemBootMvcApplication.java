03c25331193a28ba6a10d7da5dcc0f53f1f4fd62
jayesh.gaur <jayesh.gaur@somaiya.edu>
2019-10-06 13:19:57 +0530

    hcsbootmvc

---
+++ HealthCareSystemBootMVC/src/main/java/com/cg/healthcaresystembootmvc/HealthCareSystemBootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cg.healthcaresystembootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareSystemBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareSystemBootMvcApplication.class, args);
+	}
+
+}
