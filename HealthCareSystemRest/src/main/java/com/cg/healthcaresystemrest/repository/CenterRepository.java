20d1e926f920018ca9bb0ad04a70cea76629b1a6
jayesh.gaur <jayesh.gaur@somaiya.edu>
2019-10-13 19:26:50 +0530

    rest project repo and service classes

---
+++ HealthCareSystemRest/src/main/java/com/cg/healthcaresystemrest/repository/CenterRepository.java
@@ -0,0 +1,17 @@
+package com.cg.healthcaresystemrest.repository;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.cg.healthcaresystemrest.dto.DiagnosticCenter;
+
+
+
+public interface CenterRepository extends JpaRepository<DiagnosticCenter,BigInteger> {
+
+	@Query("FROM DiagnosticCenter WHERE isDeleted = 'false'")
+	public List<DiagnosticCenter> getCenterList();
+}
---
+++ HealthCareSystemRest/src/main/java/com/cg/healthcaresystemrest/repository/TestRepository.java
@@ -0,0 +1,19 @@
+package com.cg.healthcaresystemrest.repository;
+
+import java.math.BigInteger;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cg.healthcaresystemrest.dto.Test;
+
+
+/*
+ * Author : Nidhi
+ * Description : This inteface will perform CRUD operations for Test Entity.
+ * Created Date : 9th October,2019 
+ * 
+ * */
+
+public interface TestRepository extends JpaRepository<Test,BigInteger> {
+
+}
